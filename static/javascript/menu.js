9d36e0a35b15feb1b15adc0db573f65ee8c1254c
rotfuchs <jannes.schuett@schunternet.de>
2024-07-16 16:32:51 +0200

    added animation to menu

--- static/javascript/menu.js
+++ static/javascript/menu.js
@@ -1,18 +1,4 @@
-let collapsible_state = false;
-
-print = console.log
-
-const toggle_collapse = document.getElementById("toggle-collapse");
-const collapsible = document.getElementById("collapsible");
-const mobile_nav = document.getElementById("mobile-nav");
-
-toggle_collapse.onclick = function() {
-    if (collapsible_state) {
-        collapsible.style.display = "none";
-        collapsible_state = !collapsible_state;
-    } else {
-        collapsible.style.display = "block";
-        collapsible_state = !collapsible_state;
-    }
+function toggleMenu() {
+  var menu = document.getElementById("collapsibleMenu");
+  menu.classList.toggle("expanded");
 }
-
